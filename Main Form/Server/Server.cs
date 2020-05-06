d0455c70669c9aa3ff5ea4c9879aeb9adc241398
legacygoof <7kphans@gmail.com>
2020-05-06 01:07:40 -0500

    - Added Packet Writers and Readers - Made a List for current users on server - Started Setting up Server variables

---
+++ Main Form/Server/Server.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        #region server variables
+        public string clientVersion = "1.0";
+        public string adminVersion = "admin_1.0";
+        public bool connectionsOpen = true;
+        private byte[] g_buffer = new byte[1024];
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Users> userList = new List<Users>();
+        List<Admins> adminList = new List<Admins>();
+        #endregion
+    }
+}
---
+++ Main Form/Server/Utils/Flags.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+
+public enum ProcessCodes : ushort
+{
+    Login,
+    Register,
+    Fail
+}
+
+public enum ErrorCodes : ushort
+{
+    Login_Success,
+    Exists,
+    Invalid_Login,
+    Banned,
+    Token_Used,
+    Is_Logged_In,
+    Version_Success,
+    Erorr
+}
